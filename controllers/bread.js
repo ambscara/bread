1a256968a6463fc4439da51adf37db6c7e4d860e
ambscara <150319615+ambscara@users.noreply.github.com>
2024-03-05 21:10:14 -0800

    updated router post

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const Bread = require('../models/bread')
 
+//GET retrieve all the bread
 router.get('/', (req, res) => {
     res.render('index', {
         breads: Bread
@@ -8,9 +9,29 @@ router.get('/', (req, res) => {
 })
 
 
+router.get('/new', (req, res) => {
+    res.render('new')
+})
+
+// GET retrieve bread by index
 router.get('/:index', (req, res) => {
     const { index } = req.params
-    res.send(Bread[index])
+    res.render('show', {
+        bread: Bread[index]
+    })
+})
+
+
+// CREATE
+router.post('/', (req, res) => {
+    if (!req.body.image) req.body.image = 'https://houseofnasheats.com/wp-content/uploads/2022/02/French-Bread-1.jpg'
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread.push(req.body)
+    res.redirect('/bread')
 })
 
 
